84847615aa8fdd49f042f12afbff63dcac9716ea
Edu <eduardo.inca@alphatecscientific.com>
2021-07-08 17:21:42 -0500

    ss

--- pantallas/AgendaCita.js
+++ pantallas/AgendaCita.js
@@ -10,7 +10,9 @@ import {
 
 const AgendaCita = ({ navigation }) => {
     return(
-        <View><Text>Hola Diego</Text></View>
+        <View><Text>Hola Diego</Text>
+        <Text>Hola jose</Text></View>
+
     );
 }
 
