90abf1f2fcd2cc9c7024546bcbb49943ff9c8710
Victor <diestraespiritu@gmail.com>
2021-07-22 18:43:20 -0500

    New

--- pantallas/Compras.js
+++ pantallas/Compras.js
@@ -7,7 +7,7 @@ const Compras = ({ route }) => {
     <View style={styles.container}>
       <View style={styles.subcontainer}>
         <Text style={{color:'white'}}>AnalyzerId: {AnalyzerId}</Text>
-        <Text style={{color:'white'}}>OptionId--modificado: {OptionId}</Text>
+        <Text style={{color:'white'}}>OptionId--diego: {OptionId}</Text>
       </View>
     </View>
   );
